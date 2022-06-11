ed1047024b3fd9354024d870fec4081ade169c1b
Divyanshu Ranjan Srivastava <greatdrs@gmail.com>
2022-06-11 15:15:28 +0530

    completed inbox upgrade

---
+++ src/deploy.js
@@ -0,0 +1,28 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+
+const { abi, evm } = require('./compile');
+
+const provider = new HDWallerProvider(
+    'canyon license wrist swear lonely orchard raw moon fame boring space delay',
+    'https://rinkeby.infura.io/v3/257b2d2f1b64440da816529a4abd6657'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(abi)
+        .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
+};
+
+deploy();
+
+
